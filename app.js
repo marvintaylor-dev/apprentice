c9930da062df63181c80cc106b4a3e15698a9ba6
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-03-19 18:47:12 -0700

    commit 3 - Mar 19

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ const engine = require('ejs-mate');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./models/user');
-const Mentor = require('./models/user')
+const Mentor = require('./models/user');
+const methodOverride = require('method-override');
+const tiers = ['1', '2', '3'];
+const fields = ['Psychology', 'Engineering', 'Biology', 'Physics', 'Arts', 'Trades', 'Content-Creation', 'Business'];
+const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+    'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+    'Wisconsin', 'Wyoming']
 
 
 //connects Mongoose to Mongo database
 mongoose.connect('mongodb://localhost:27017/apprentice', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(() => {
         console.log("MONGO CONNECTION OPEN")
@@ -37,6 +47,8 @@ db.once("open", () => {
 //route to public folder
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+//allows you to use ?method_ to turn POST requests into PUT / PATCH requests
+app.use(methodOverride('_method'))
 
 app.engine('ejs', engine);
 
@@ -46,54 +58,95 @@ app.set('views', path.join(__dirname, 'views'));
 //allows me to parse information from the body of the POST request
 app.use(express.urlencoded({ extended: true }))
 
-
+//home page
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+//login page
 app.get('/login', (req, res) => {
     res.render('users/login')
 })
 
+//choose a path page
 app.get('/signup', (req, res) => {
     res.render('users/signup')
 })
 
-app.get('/mentee', (req, res) => {
+//mentee sign up page
+app.get('/mentee/new', (req, res) => {
     res.render('users/mentee')
 })
 
-app.get('/mentor', (req, res) => {
-    res.render('users/mentor')
+//mentor sign up page
+app.get('/mentor/new', (req, res) => {
+    res.render('users/mentor', { fields, states })
 })
 
+//create a new mentor
+app.post('/list', async (req, res) => {
+    const newMentor = new Mentor(req.body);
+    await newMentor.save()
+    console.log(req.body)
+    return res.redirect('/list')
+})
+
+//mentee vs mentor page
 app.get('/faq', (req, res) => {
     res.render('explore/faq')
 })
 
-app.get('/explore', (req, res) => {
-    res.render('explore/explore')
-})
-
-app.get('/list', async (req, res) => {
+//show the list of academic subjects
+app.get('/explore', async (req, res) => {
     const mentors = await Mentor.find({})
-    res.render('explore/list', { mentors })
+    res.render('explore/explore', { fields, mentors })
+})
+
+//show the list of mentors
+app.get('/list', async (req, res) => {
+    const { field } = req.query
+    if (field === fields) {
+        const mentors = await Mentor.find({ field })
+        return res.render('explore/list', { mentors })
+    } else {
+        const mentors = await Mentor.find({})
+        return res.render('explore/list', { mentors })
+    }
 })
 
 
+
+//Mentor page from Mentee point of view
 app.get('/explore/:id', async (req, res) => {
     const { id } = req.params
     const mentor = await Mentor.findById(id);
     res.render('explore/show', { mentor })
 })
 
+//need access to profile in order to edit information
+
+//tiers page explaining what tiers are 
 app.get('/tiers', (req, res) => {
     res.render('explore/tiers')
 })
 
-app.post('/tiers', (req, res) => {
-    console.log(req.body)
-    res.send('new mentor')
+app.get('/restricted/:id', async (req, res) => {
+    const { id } = req.params
+    const mentor = await Mentor.findById(id);
+    return res.render('restricted/profile', { mentor, fields, states, tiers })
+})
+
+app.put('/restricted/:id', async (req, res) => {
+    const { id } = req.params
+    const edited = await Mentor.findByIdAndUpdate(id, req.body,
+        { runValidators: true, new: true })
+    return res.redirect('/list')
+})
+
+app.delete('/restricted/:id', async (req, res) => {
+    const { id } = req.params
+    const userDeleted = await Mentor.findByIdAndDelete(id)
+    return res.redirect('/list')
 })
 
 app.listen(3000, () => {
--- models/user.js
+++ models/user.js
@@ -75,8 +75,41 @@ const mentorSchema = new Schema({
             required: true
         }
     },
+    email: {
+        type: String,
+    },
+    username: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    age: {
+        type: String,
+    },
+    tier: {
+        type: String,
+        enum: ['1', '2', '3']
+    },
     salary: {
         type: String,
+    },
+    location: {
+        city: {
+            type: String
+        },
+        state: {
+            type: String,
+            enum: ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
+                'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+                'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+                'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+                'Wisconsin', 'Wyoming']
+        },
+        zip: {
+            type: Number
+        }
     },
     job_description: {
         type: String
@@ -85,7 +118,8 @@ const mentorSchema = new Schema({
         type: String
     },
     field_of_study: {
-        type: String
+        type: String,
+        enum: ['Psychology', 'Engineering', 'Biology', 'Physics', 'Arts', 'Trades', 'Content-Creation', 'Business']
     }
 })
 
