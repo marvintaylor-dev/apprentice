b60a5ee0e37b40bcae0c366d2f8f49b87bc9ec58
Marvin Taylor <marvintaylor@Marvins-MacBook-Pro.local>
2021-03-23 14:25:58 -0700

    App.js readability and middleware update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//connection to express.js
 const express = require('express');
 const app = express();
 //allows you to connect views to directories within views
@@ -6,10 +7,16 @@ const path = require('path');
 const engine = require('ejs-mate');
 //provides syntax to pass information into our Mongo database
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const User = require('./models/user');
+//provides a log for activity such as log in.
+const morgan = require('morgan')
+
+//access to our Mentor user model
 const Mentor = require('./models/user');
+
+//allows us to use PUT and DELETE requests 
 const methodOverride = require('method-override');
+
+//form information in the 'select' categories.
 const tiers = ['1', '2', '3'];
 const fields = ['Psychology', 'Engineering', 'Biology', 'Physics', 'Arts', 'Trades', 'Content-Creation', 'Business'];
 const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
@@ -18,6 +25,9 @@ const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Color
     'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
     'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
     'Wisconsin', 'Wyoming']
+
+
+//-----------MONGO / MONGOOSE DB CONNECTION-------------
 
 
 //connects Mongoose to Mongo database
@@ -34,7 +44,6 @@ mongoose.connect('mongodb://localhost:27017/apprentice', {
         console.log('OH NO, MONGO CONNECTION ERROR!!')
         console.log(err)
     })
-
 //confirm database connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -43,13 +52,15 @@ db.once("open", () => {
 });
 
 
+//--------------------MIDDLEWARE--------------------
 
-//route to public folder
+
+//Tells express to serve static files and parse the body as JSON
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 //allows you to use ?method_ to turn POST requests into PUT / PATCH requests
 app.use(methodOverride('_method'))
-
+//allows us to use the ejs engine and syntax with .ejs files
 app.engine('ejs', engine);
 
 app.set('view engine', 'ejs');
@@ -57,6 +68,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 //allows me to parse information from the body of the POST request
 app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+
+
+//-------------------WEB PAGES-----------------
+
 
 //home page
 app.get('/', (req, res) => {
@@ -115,7 +132,6 @@ app.get('/list', async (req, res) => {
 })
 
 
-
 //Mentor page from Mentee point of view
 app.get('/explore/:id', async (req, res) => {
     const { id } = req.params
@@ -123,19 +139,19 @@ app.get('/explore/:id', async (req, res) => {
     res.render('explore/show', { mentor })
 })
 
-//need access to profile in order to edit information
-
 //tiers page explaining what tiers are 
 app.get('/tiers', (req, res) => {
     res.render('explore/tiers')
 })
 
+//Mentor profile access - should be restricted by permissions
 app.get('/restricted/:id', async (req, res) => {
     const { id } = req.params
     const mentor = await Mentor.findById(id);
     return res.render('restricted/profile', { mentor, fields, states, tiers })
 })
 
+//mentor additional profile information
 app.put('/restricted/:id', async (req, res) => {
     const { id } = req.params
     const edited = await Mentor.findByIdAndUpdate(id, req.body,
@@ -143,11 +159,21 @@ app.put('/restricted/:id', async (req, res) => {
     return res.redirect('/list')
 })
 
+//deletion of a mentor
 app.delete('/restricted/:id', async (req, res) => {
     const { id } = req.params
     const userDeleted = await Mentor.findByIdAndDelete(id)
     return res.redirect('/list')
 })
+
+app.use((req, res) => {
+    res.status(404)
+    res.send('Not Found')
+})
+
+
+//---------------------LOCAL CONNECTION-----------------
+
 
 app.listen(3000, () => {
     console.log("App is listening on Port 3000!")
