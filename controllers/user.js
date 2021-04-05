aa06d54c990361fefd42df1c93110e780ae5ed1d
Marvin Taylor <marvintaylor@Marvins-MacBook-Pro.local>
2021-04-05 11:51:58 -0700

    sign up salt/hash and login auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,20 @@
 
 const User = require("../models/user");
-
+const passport = require('passport');
+const { nextTick } = require("process");
 
 //login page
 module.exports.loginPage = (req, res) => {
     res.render('users/login')
 }
 
-/* module.exports.userLogin = async (req, res) => {
-    const { username, em }
+//login authentication
+module.exports.userLogin = async (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/explore';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 }
- */
 
 //User sign up page
 module.exports.userSignup = (req, res) => {
@@ -24,16 +28,22 @@ module.exports.selectPath = (req, res) => {
 }
 //create a new mentor
 module.exports.createUser = async (req, res) => {
-    const newUser = new User(req.body);
-    await newUser.save()
-    console.log(req.body)
-    req.flash('success', 'Successfully created User!')
-    return res.redirect('/path')
+    try {
+        const { email, username, password } = req.body
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return nextTick(err);
+            req.flash('success', 'Successfully created User!')
+            console.log(user)
+            return res.redirect('/path');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        return res.redirect('signup');
+    }
+
+
+
 }
-
-
-//mentee sign up page
-/* module.exports.menteeSignup = (req, res) => {
-    res.render('users/mentee')
-} */
 
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const app = express();
 const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync")
 //custom Error Handler
 const ExpressError = require('../utils/ExpressError')
 //access to our User model
 const User = require('../models/user');
-//access to Review model
-const Review = require('../models/review');
 const user = require('../controllers/user')
 const { validateMentor, validateReview } = require('../middleware.js')
+const passport = require('passport')
 
 
 router.get('/login', user.loginPage)
-/* router.post('/login/:id', user.userLogin) */
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), user.userLogin)
 router.get('/signup', user.userSignup)
 router.get('/path', user.selectPath)
 router.post('/path', validateMentor, catchAsync(user.createUser))
