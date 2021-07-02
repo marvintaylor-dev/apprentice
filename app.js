c36b48bc97c24cd6e6fa274b757a5e8af3c00abc
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-07-01 20:53:19 -0700

    mongoose connection to dbUrl

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/apprentice'
 
 //-----------MONGO / MONGOOSE DB CONNECTION-------------
 
-mongoose.connect('mongodb://localhost:27017/apprentice', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
