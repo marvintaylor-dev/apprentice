fd8c124d4322ea8b51c7111fde9f8a83d0d20561
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-11-03 22:17:54 -0700

    added remove button to notes

--- public/javascripts/notes.js
+++ public/javascripts/notes.js
@@ -7,9 +7,20 @@ const noteList = document.querySelector('.noteList');
 
 button.addEventListener('click', (e) => {
     e.preventDefault()
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "&#10008;"; /* <-- x */
+    removeButton.addEventListener("click", function () {
+        noteList.removeChild(noteContainer);
+    })
+    removeButton.classList.add('remove-button');
+
     const noteContainer = document.createElement('li');
-    const dash = noteContainer.append('-- ')
-    const note = noteContainer.append(noteInput.value)
+    noteContainer.append(noteInput.value);
+    noteContainer.append(removeButton);
+
     noteContainer.classList.add('note')
     noteList.append(noteContainer);
+
+    noteInput.value = "";
 })
