4fedd76dfe52409daed071a860b493e8775c6eb7
Marvin Taylor <marvintaylor@Marvins-iMac.local>
2021-11-09 21:52:02 -0800

    added edit button to notes

--- public/javascripts/notes.js
+++ public/javascripts/notes.js
@@ -10,15 +10,30 @@ button.addEventListener('click', (e) => {
     e.preventDefault()
 
     const removeButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    const buttonContainer = document.createElement("div");
+    const editInput = document.createElement("input");
+
     removeButton.innerHTML = "&#10008;"; /* <-- x */
     removeButton.addEventListener("click", function () {
         noteList.removeChild(noteContainer);
     })
     removeButton.classList.add('remove-button');
 
+    editButton.innerHTML = "&#9998;"
+    editButton.addEventListener("click", function () {
+        noteList.replaceChild(noteContainer);
+    })
+    editButton.classList.add('edit-button');
+
+    buttonContainer.append(editButton);
+    buttonContainer.append(removeButton);
+    buttonContainer.classList.add('button-container');
+
     const noteContainer = document.createElement('li');
     noteContainer.append(noteInput.value);
-    noteContainer.append(removeButton);
+    noteContainer.append(buttonContainer);
+
 
     noteContainer.classList.add('note')
     noteList.append(noteContainer);
